fe4e86739e3fd18faef2e60517256acfbb4cc957
Anjanasbr2003 <anjanasbr2003@gmail.com>
2026-03-12 13:20:10 +0530

    English units form

---
+++ src/EnglishData.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class EnglishData extends JFrame{
+    private JLabel weightLabel;
+    private JTextField weightInput;
+    private JLabel heightLabel;
+    private JTextField heightInput;
+    private JButton calculateButton;
+    private JPanel englishPanel;
+
+    public EnglishData(){
+        //set
+        setContentPane(englishPanel);
+    }
+}
